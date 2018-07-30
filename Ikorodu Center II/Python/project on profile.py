607e0cdc8c5fb9bd3a2e225b1e88e66c87c2980b
Rexben <30627428+Rexben001@users.noreply.github.com>
2018-07-31 00:26:05 +0100

    Add files via upload

---
+++ Ikorodu Center II/Python/project on profile.py
@@ -0,0 +1,20 @@
+import string
+string.ascii_lowercase
+'abcdefghijklmnopqrstuvwxyz'
+
+id_user=str(input('what is your name ?'))
+sex= str(input('enter M for male or F for female '))
+if sex=='m' or 'F' or sex=='f' or 'M':
+   pass
+else:
+   str(input('enter M or F'))
+phone= int(input('enter number '))
+age=float(input('must be 18 years and above'))
+if age>=18:
+   print(id_user,
+   	sex,
+   	age,
+   	phone,
+   	'WELCOME'+ id_user)
+else:
+   print('you are not eligible')
